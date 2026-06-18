64907b9b12421be3d4c742409743d7af32aedd4f
Jon Barber <jon.barber@riskledger.com>
2026-06-18 13:04:46 +0100

    Set Atuin auto_sync, filter_mode, and sync records

--- modules/home-manager/atuin.nix
+++ modules/home-manager/atuin.nix
@@ -7,5 +7,13 @@
     enable = true;
     package = pkgs.unstablePkgs.atuin;
     enableFishIntegration = true;
+
+    settings = {
+      auto_sync = false;
+      filter_mode = "session-preload";
+      sync = {
+        records = true;
+      };
+    };
   };
 }
