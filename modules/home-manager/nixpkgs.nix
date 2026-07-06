9c32faf93e890de671deca19353fb740364848ae
Jon Barber <jon.barber@riskledger.com>
2026-07-06 14:27:16 +0100

    Update flake.lock and disable pinned-mise overlay

--- modules/home-manager/nixpkgs.nix
+++ modules/home-manager/nixpkgs.nix
@@ -5,7 +5,7 @@
       inputs.self.overlays.additions
       inputs.self.overlays.modifications
       inputs.self.overlays.unstable-packages
-      inputs.self.overlays.pinned-mise
+      #inputs.self.overlays.pinned-mise
       inputs.llm-agents.overlays.default
     ];
     config = {
