c3077f9965a360942219a7e3acb1473ac8036143
Jon Barber <jon.barber@acm.org>
2026-09-26 14:03:30 +0100

    Enable nix-direnv for cached use flake/use nix in .envrc

--- modules/home-manager/direnv.nix
+++ modules/home-manager/direnv.nix
@@ -7,6 +7,9 @@
     enable = true;
     package = pkgs.direnv;
 
+    # Cached, GC-rooted `use flake` / `use nix` in .envrc files.
+    nix-direnv.enable = true;
+
     # mise = {
     #   enable = true;
     #   package = pkgs.unstablePkgs.mise;
